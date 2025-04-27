22265a8a05bd2698648c8d5d9fa972caa6b44fd0
nemitha2005 <nemithan05@gmail.com>
2025-04-27 18:19:42 +0530

    feat: add flow network model

---
+++ src/com/networkflow/model/FlowNetwork.java
@@ -0,0 +1,60 @@
+package com.networkflow.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlowNetwork {
+    private List<Node> nodes;
+    private List<Edge> edges;
+    private int sourceId;
+    private int targetId;
+
+    public FlowNetwork(int numberOfNodes) {
+        nodes = new ArrayList<>(numberOfNodes);
+        edges = new ArrayList<>();
+
+        for (int i = 0; i < numberOfNodes; i++) {
+            nodes.add(new Node(i));
+        }
+
+        sourceId = 0;
+        targetId = numberOfNodes - 1;
+    }
+
+    public Edge addEdge(int from, int to, int capacity) {
+        Node fromNode = nodes.get(from);
+        Node toNode = nodes.get(to);
+
+        Edge edge = new Edge(fromNode, toNode, capacity);
+
+        fromNode.addOutgoingEdge(edge);
+        toNode.addIncomingEdge(edge);
+        edges.add(edge);
+
+        return edge;
+    }
+
+    public Node getSource() {
+        return nodes.get(sourceId);
+    }
+
+    public Node getTarget() {
+        return nodes.get(targetId);
+    }
+
+    public List<Node> getNodes() {
+        return nodes;
+    }
+
+    public Node getNode(int id) {
+        return nodes.get(id);
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+
+    public int getNumberOfNodes() {
+        return nodes.size();
+    }
+}
